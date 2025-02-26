529c35051805d430fc45da8057c7dfa916a03cd9
Marcos Magaña <imarcosmagana@gmail.com>
2025-02-25 21:23:36 -0600

    promesas

--- 02-intro-javascript/src/bases/08-import-export.js
+++ 02-intro-javascript/src/bases/08-import-export.js
@@ -1,17 +1,17 @@
 //import { heroes } from './data/heroes.js'
 //import {heroes} from './data/heroes'
 
-import { heroes } from './data/heroes'
+import { heroes } from '../data/heroes'
 
-console.log(heroes);
+//console.log(heroes);
 
 
-const getHeroesById = (id) => {
+export const getHeroesById = (id) => {
     return heroes.find( (heroes) => heroes.id === id);
 }
 
-const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
+export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
 
-console.log( getHeroesById(2) );
+//console.log( getHeroesById(2) );
 
-console.log( getHeroesByOwner('Marvel') );
+//console.log( getHeroesByOwner('Marvel') );
