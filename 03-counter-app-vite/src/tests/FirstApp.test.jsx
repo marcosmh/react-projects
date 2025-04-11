c902f77e29da64da4870fd73c78a87d1d80d40b3
Marcos Magaña <imarcosmagana@gmail.com>
2025-04-11 11:45:20 -0600

    correccion de archivos de configuracion jest

--- 03-counter-app-vite/src/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/src/tests/FirstApp.test.jsx
@@ -1,16 +1,15 @@
 import { render } from '@testing-library/react';
 import { FirstApp } from '../FirstApp';
 
+
 describe('TEST en <FirstApp/>: ', () => {
 
 
     test('Debe hacer match con el snapshot... ', () => {
 
-        console.log('Test env:', typeof document !== 'undefined' ? 'jsdom' : 'node');
-
         const title = 'Hola, Markcode';
         const { container } = render( <FirstApp title = { title } /> );
-        
+
         console.log( container );
       
     });
