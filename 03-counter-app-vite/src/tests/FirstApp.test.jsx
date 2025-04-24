cc808845cc4b2694e152ff503bb4cb6eb6bfba40
Marcos Magaña <imarcosmagana@gmail.com>
2025-04-24 10:46:30 -0600

    pruebas unitarias 2

--- 03-counter-app-vite/src/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/src/tests/FirstApp.test.jsx
@@ -10,8 +10,25 @@ describe('TEST en <FirstApp/>: ', () => {
         const title = 'Hola, Markcode';
         const { container } = render( <FirstApp title = { title } /> );
 
-        console.log( container );
+        //console.log( container );
+
+        expect( container ).toMatchSnapshot();
       
+    });
+    
+    test('Debe mostrar el título en un h1: ', () => {
+        const title = 'Hola, Markcode';
+        const { container, getByText } = render( <FirstApp title = { title } /> );
+
+        expect( getByText( title ) ).toBeTruthy();
+
+        const h1 = container.querySelector( 'h1' );
+
+        console.log( h1.innerHTML );
+
+        expect( h1.innerHTML ).toBe(title);
+
+        expect( h1.innerHTML ).toContain(title);
     });
     
   
