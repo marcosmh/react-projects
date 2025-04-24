c85134f8f76dc239ce42191895bdc9b3b950260f
Marcos Magaña <imarcosmagana@gmail.com>
2025-04-24 11:33:41 -0600

    pruebas unitarias 3

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -15,7 +15,9 @@ function getSaludo() {
 export const FirstApp = ( { title, subTitle, name }  ) => {
     return (
         <>
-            <h1>{ title }</h1>
+            {/* <h1>{ title }</h1> */}
+            <div data-testid="test-title">{ title }</div>
+            <p>{ subTitle }</p>
             <p>{ subTitle }</p>
             <p>{ name }</p>
         </>
--- 03-counter-app-vite/src/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/src/tests/FirstApp.test.jsx
@@ -4,32 +4,49 @@ import { FirstApp } from '../FirstApp';
 
 describe('TEST en <FirstApp/>: ', () => {
 
-
+    /*
     test('Debe hacer match con el snapshot... ', () => {
 
         const title = 'Hola, Markcode';
         const { container } = render( <FirstApp title = { title } /> );
-
         //console.log( container );
-
         expect( container ).toMatchSnapshot();
       
     });
+    */
     
     test('Debe mostrar el título en un h1: ', () => {
         const title = 'Hola, Markcode';
-        const { container, getByText } = render( <FirstApp title = { title } /> );
-
+        const { container, getByText, getByTestId } = render( <FirstApp title = { title } /> );
         expect( getByText( title ) ).toBeTruthy();
 
-        const h1 = container.querySelector( 'h1' );
+        // const h1 = container.querySelector( 'h1' );
+        // console.log( h1.innerHTML );
+        // expect( h1.innerHTML ).toBe(title);
+        // expect( h1.innerHTML ).toContain(title);
 
-        console.log( h1.innerHTML );
+        expect( getByTestId( 'test-title') ).toBeTruthy();
+        expect( getByTestId( 'test-title').innerHTML ).toBe( title );
+        expect( getByTestId( 'test-title').innerHTML ).toContain( title );
 
-        expect( h1.innerHTML ).toBe(title);
-
-        expect( h1.innerHTML ).toContain(title);
     });
+
+    test('Debe de mostrar el subtitulo enviado por props ', () => {
+        const title = 'Hola, Markcode';
+        const subTitle = 'Soy un subtitulo';
+        const { getAllByText } = render( 
+            <FirstApp 
+                title = { title }
+                subTitle = { subTitle } 
+            /> 
+        );
+        expect( getAllByText( subTitle ).length ).toBe(2);
+
+
+
+
+    })
+    
     
   
 });
