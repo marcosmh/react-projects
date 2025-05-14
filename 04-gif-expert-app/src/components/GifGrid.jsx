5ac829cf244545ba84a8689ad1c9f8eaf9473f96
Marcos Magaña <imarcosmagana@gmail.com>
2025-05-13 18:30:09 -0600

    useEffect modificacion

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,9 +1,7 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-
-  const [counter, setCounter] = useState(10);
 
   useEffect( () => {
     getGifs( category );
@@ -13,8 +11,6 @@ export const GifGrid = ({ category }) => {
   return (
     <>
         <h3>{ category } </h3>
-        <h5>{ counter } </h5>
-        <button onClick={ () => setCounter( counter + 1) }>+ 1</button>
     </>
   )
 
