f781bda7cde956f0461766439f7ff0f72ba0de9a
Marcos Magaña <imarcosmagana@gmail.com>
2025-06-03 21:50:05 -0600

    hook - useState

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HookApp } from './HookApp';
+
+//import { HookApp } from './HookApp';
+import { CounterApp } from './01-useState/CounterApp';
 
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HookApp />
-  </StrictMode>,
+    <CounterApp />
+  </StrictMode>
 )
